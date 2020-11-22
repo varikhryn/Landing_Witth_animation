eea5743b0941dd06fa1d4c5b799f002892c57645
varikhryn <y.yarikhryn@gmail.com>
2020-11-22 19:08:24 +0200

    all project without mobile version

---
+++ script/JQuery.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // slider slick
+    $('#slider-m-partners').slick({
+        autoSlidesToShow: true,
+        speed: 300,
+        variableWidth: true,
+        slidesToScroll: 1,
+        infinite: true,
+        slidesToShow: 4,
+        prevArrow: $('#btn_partners_prev'),
+        nextArrow: $('#btn_partners_next'),
+        // rows: 0,
+    });// ./ slider slick
+
+    // slider slick proud
+    $('#m-proud_slider').slick({
+        dots: false,
+        infinite: true,
+        lazyLoad: 'ondemand',
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: $('#btn_proud_prev'),
+        nextArrow: $('#btn_proud_next'),
+        // rows: 0,
+    });// ./ slider slick proud
+
+    // hover effect on btn start
+    $(".button_su_inner").mouseenter(function (e) {
+        var parentOffset = $(this).offset();
+        var relX = e.pageX - parentOffset.left;
+        var relY = e.pageY - parentOffset.top;
+        $(this).prev(".su_button_circle").css({ "left": relX, "top": relY });
+        $(this).prev(".su_button_circle").removeClass("desplode-circle");
+        $(this).prev(".su_button_circle").addClass("explode-circle");
+    });
+
+    $(".button_su_inner").mouseleave(function (e) {
+        var parentOffset = $(this).offset();
+        var relX = e.pageX - parentOffset.left;
+        var relY = e.pageY - parentOffset.top;
+        $(this).prev(".su_button_circle").css({ "left": relX, "top": relY });
+        $(this).prev(".su_button_circle").removeClass("explode-circle");
+        $(this).prev(".su_button_circle").addClass("desplode-circle");
+    });// ./ hover effect on btn start
+});
---
+++ script/script.js
@@ -0,0 +1,174 @@
+// this block add class when block is active and user can see this block
+let animItems;
+if (document.querySelectorAll('._anim-items') != null && document.querySelectorAll('._anim-items') != undefined) {
+    animItems = document.querySelectorAll('._anim-items');
+}
+let animItemsTranslate;
+if (document.querySelectorAll('._anim-items-translate') != null && document.querySelectorAll('._anim-items-translate') != undefined) {
+    animItemsTranslate = document.querySelectorAll('._anim-items-translate');
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // ***********  add animation on full site ... START ... ***********
+    window.addEventListener('scroll', function () {
+        animOnScroll();
+        animOnScrollTranslate();
+
+        let hedaerFix;
+        if (document.getElementById('header-fix') != null && document.getElementById('header-fix') != undefined) {
+            hedaerFix = document.getElementById('header-fix');
+            hedaerFix.hidden = (pageYOffset < (document.documentElement.clientHeight + 74));
+            hedaerFix.style.opacity = 1;
+            hedaerFix.style.zIndex = 99999999;
+            hedaerFix.style.transition = "all .5s ease-in";
+        }
+    });
+    if (pageYOffset > (document.documentElement.clientHeight + 74)) {
+        let hedaerFix;
+        if (document.getElementById('header-fix') != null && document.getElementById('header-fix') != undefined) {
+            hedaerFix = document.getElementById('header-fix');
+            hedaerFix.hidden = (pageYOffset < (document.documentElement.clientHeight + 74));
+            hedaerFix.style.opacity = 1;
+            hedaerFix.style.zIndex = 99999999;
+            hedaerFix.style.transition = "all .5s ease-in";
+        }
+    }
+
+
+    setTimeout(() => {
+        animOnScroll();
+
+        animOnScrollTranslate();
+    }, 300);
+    // ***********  add animation on full site ... END ... ***********
+
+
+    // lang in section 100vh
+    let btnHLang;
+    if (document.getElementById('btn_s-100vh') != null && document.getElementById('btn_s-100vh') != undefined) {
+        btnHLang = document.getElementById('btn_s-100vh');
+        btnHLang.onclick = function () {
+            this.classList.toggle('active_100vh_lng');
+            document.body.classList.toggle('active_100vh_lang');
+        }
+    }
+    // lang in header fixed
+    // btn_header-fixed_lang
+    let btnLangHFixed;
+    if (document.getElementById('btn_header-fixed_lang') != null && document.getElementById('btn_header-fixed_lang') != undefined) {
+        btnLangHFixed = document.getElementById('btn_header-fixed_lang');
+        btnLangHFixed.onclick = function () {
+            this.classList.toggle('active_h-f_lng');
+            document.body.classList.toggle('active_h_fixed_lng');
+        }
+    }
+
+    // all see event that close when click other blocks 
+    document.addEventListener('click', function (e) {
+
+        // choose lang in 100vh
+        if (document.body.classList.contains('active_100vh_lang') == true) {
+            let s100Lang;
+            if (document.getElementById('btn_s-100vh') != null && document.getElementById('btn_s-100vh') != undefined) {
+                s100Lang = document.getElementById('btn_s-100vh');
+                if (!s100Lang.contains(e.target)) {
+                    s100Lang.classList.remove('active_100vh_lng');
+                    document.body.classList.remove('active_100vh_lang');
+                }
+            }
+        }
+
+        // choose lang in header fixed
+        if (document.body.classList.contains('active_h_fixed_lng') == true) {
+            let HFLang;
+            if (document.getElementById('btn_header-fixed_lang') != null && document.getElementById('btn_header-fixed_lang') != undefined) {
+                HFLang = document.getElementById('btn_header-fixed_lang');
+                if (!HFLang.contains(e.target)) {
+                    HFLang.classList.remove('active_h-f_lng');
+                    document.body.classList.remove('active_h_fixed_lng');
+                }
+            }
+        }
+    });
+
+    // tab on main page for services
+    let btnsMainServices;
+    if (document.querySelectorAll(".m_about-item_this-btn") != null && document.querySelectorAll(".m_about-item_this-btn") != undefined) {
+        btnsMainServices = document.querySelectorAll(".m_about-item_this-btn");
+        for (let item of btnsMainServices) {
+            item.onclick = function () {
+                this.parentElement.parentElement.parentElement.classList.toggle('active-tab-service');
+                animOnScroll()
+            }
+        }
+    }
+});
+
+// ***********  add animation on full site ... START ... ***********
+function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 2;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add('_active-scrll-anim');
+        } else {
+            animItem.classList.remove('_active-scrll-anim');
+        }
+    }
+}
+
+function animOnScrollTranslate() {
+
+    for (let index = 0; index < animItemsTranslate.length; index++) {
+        const animItem = animItemsTranslate[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 8;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            console.log("work")
+            animItem.style.transform = offsetTranslate(animItem).transform;
+        }
+    }
+}
+
+function offsetTranslate(el) {
+
+    const rect = el.getBoundingClientRect();
+    console.log("****")
+    console.log(rect.y)
+
+    let k = el.clientLeft + (el.offsetHeight - rect.y) * 2;
+    let z = { transform: "translateX(" + k + "px)", };
+    if (k <= el.clientLeft) {
+        z = { transform: "translateX(" + el.clientLeft + "px)", };
+    } else {
+        z = { transform: "translateX(" + k + "px)", }
+    }
+    // console.log(z);
+    return z;
+}
+
+function offset(el) {
+    const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}// ***********  add animation on full site ... END ... ***********
